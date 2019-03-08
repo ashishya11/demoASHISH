e33690b74742a2b93c973efcd6213c83fec3a1a6
Ashish Kumar Singh <ashishsingh.ashishya@gmail.com>
2019-03-08 17:13:05 +0530

    updated

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -9,7 +9,7 @@ export default class App extends React.Component{
             <React.Fragment>
                 <header className="header">App Header</header>
                 <section className="body">                    
-                    <h1 className="title">Hello and Welcome, React-Redux-Sass Starter kit is successfully build and your kit is ready to use</h1>
+                    <h1 className="title">Hello and Welcome, React-Redux-Sass Demo kit is successfully build and your kit is ready to use</h1>
                 </section>
                 <footer className="header">App Footer</footer>
             </React.Fragment>
